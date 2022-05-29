d2bb5c2167a75469722407018396253fe8990674
DebordCompany <debord.info@gmail.com>
2022-05-29 20:26:35 +0200

    desarrollo web-dev
    He creado la pagina web-dev
    -

--- public/js/main.js
+++ public/js/main.js
@@ -1,63 +1,53 @@
-window.onload = function(){
-    // cartel cookies
-    const privacidad = document.querySelector(".cookies");
-    
-    if(!localStorage.getItem("cookies-aceptadas") == true){
-        privacidad.classList.add("activo");
-    };
-    
-    privacidad.addEventListener("click", ocultar);
-    function ocultar (){
-        privacidad.classList.remove("activo");
-        localStorage.setItem("cookies-aceptadas", true);
-    };
-    
-    
-    // menu interactivo
-    
-    const boton = document.getElementById("accion");
-    const menu = document.getElementById("hide");
-    const quitar = document.querySelector("ul");
-    const fuera = document.querySelector(".main");
-    
-    boton.addEventListener("click", desplegar);
-    quitar.addEventListener("click", recoger);
-    fuera.addEventListener("click", recoger);
-    
-    
-    function desplegar(desplegar){
-        menu.classList.toggle("desplegable")
-        
-        
-    };
-    function recoger(recoger){
-        menu.classList.remove("desplegable");
-        console.log("funciona");
-        
-    };
-    // avance a contacto
-    
-    var contacto = document.getElementById("contacto");
-    var seccion = document.querySelector(".contacto");
-    var avance = document.querySelector(".avance");
-    
-    contacto.addEventListener("click", mover);
-    avance.addEventListener("click", mover);
-    
-    
-    function mover (){
-        seccion.scrollIntoView({behavior: 'smooth'})
-        
-        
-    };
-    // politica de privacidad
-    const minimo = document.querySelector(".minimo")
-    const texto = document.querySelector(".texto")
-    
-    minimo.addEventListener("click", abrir);
-    function abrir (){
-        texto.classList.toggle("desplegar");
-    };
-    
-    
-     };
+window.onload = function () {
+  // cartel cookies
+  const privacidad = document.querySelector(".cookies");
+
+  if (!localStorage.getItem("cookies-aceptadas") == true) {
+    privacidad.classList.add("activo");
+  }
+
+  privacidad.addEventListener("click", ocultar);
+  function ocultar() {
+    privacidad.classList.remove("activo");
+    localStorage.setItem("cookies-aceptadas", true);
+  }
+
+  // menu interactivo
+
+  const boton = document.getElementById("accion");
+  const menu = document.getElementById("hide");
+  const quitar = document.querySelector("ul");
+  const fuera = document.querySelector(".main");
+
+  boton.addEventListener("click", desplegar);
+  quitar.addEventListener("click", recoger);
+  fuera.addEventListener("click", recoger);
+
+  function desplegar(desplegar) {
+    menu.classList.toggle("desplegable");
+  }
+  function recoger(recoger) {
+    menu.classList.remove("desplegable");
+    console.log("funciona");
+  }
+  // avance a contacto
+
+  var contacto = document.getElementById("contacto");
+  var seccion = document.querySelector(".contacto");
+  var avance = document.querySelector(".avance");
+
+  contacto.addEventListener("click", mover);
+  avance.addEventListener("click", mover);
+
+  function mover() {
+    seccion.scrollIntoView({ behavior: "smooth" });
+  }
+  // politica de privacidad
+  const minimo = document.querySelector(".minimo");
+  const texto = document.querySelector(".texto");
+
+  minimo.addEventListener("click", abrir);
+  function abrir() {
+    texto.classList.toggle("desplegar");
+  }
+};
---
+++ src/js/web.js
@@ -0,0 +1,51 @@
+// cambio de texto e imagen
+
+var titular = document.querySelector("#titular");
+var titular02 = document.querySelector("#titular02");
+var titular03 = document.querySelector("#titular03");
+var titular04 = document.querySelector("#titular04");
+var titular05 = document.querySelector("#titular05");
+var imagen = document.getElementById("imagen");
+console.log(window.scrollY);
+window.onscroll = function () {
+  if (window.scrollY > 1400 && window.scrollY < 1700) {
+    titular.classList.add("aparece");
+    imagen.src = "/media/iphone-mockup.png";
+  } else if (window.scrollY > 1700) {
+    titular.classList.remove("aparece");
+  }
+  if (window.scrollY > 1700 && window.scrollY < 2000) {
+    titular02.classList.add("aparece");
+    imagen.src = "/media/ipad-mockup.png";
+  } else if (window.scrollY < 1700 || window.scrollY > 2000) {
+    titular02.classList.remove("aparece");
+  }
+  if (window.scrollY > 2000 && window.scrollY < 2300) {
+    titular03.classList.add("aparece");
+    imagen.src = "/media/iphone-mockup-design.png";
+  } else if (window.scrollY < 2000 || window.scrollY > 2300) {
+    titular03.classList.remove("aparece");
+  }
+  if (window.scrollY > 2300 && window.scrollY < 2600) {
+    titular04.classList.add("aparece");
+    imagen.src = "/media/iphone-mockup-cv.png";
+  } else if (window.scrollY < 2300 || window.scrollY > 2600) {
+    titular04.classList.remove("aparece");
+  }
+  if (window.scrollY > 2600 && window.scrollY < 2800) {
+    titular05.classList.add("aparece");
+    imagen.src = "/media/iphone-mockup-cv.png";
+  } else if (window.scrollY < 2600 || window.scrollY > 3500) {
+    titular05.classList.remove("aparece");
+  }
+};
+// movimiento a inicio
+var boton = document.querySelector("#subir");
+var inicio = document.querySelector("nav");
+
+boton.addEventListener("click", mover);
+
+function mover() {
+  inicio.scrollIntoView({ behaivor: "smooth" });
+  console.log("hola");
+}
